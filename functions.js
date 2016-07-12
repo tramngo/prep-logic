d441a7e37efa271acc6ef6ef9becd0929c0999a1
Tram Ngo <tramngo2@gmail.com>
2016-07-12 01:51:01 -0500

    complete logic functions

--- functions.js
+++ functions.js
@@ -46,6 +46,14 @@ Function - youGetTaco
 Write a function that takes a single String parameter called action. If the value passed into our function is eathave the function return the String value EAT TACOS.
 */
 
+function youGetTaco (action) {
+  if (action === "eat") {
+    return "Eat Tacos"
+  }
+}
+youGetTaco ();
+
+console.log(youGetTaco ("eat"))
 
 /*Function - isNumberGreaterThan
 Write a function that takes two variables of type Number called first and secondrespectively. Return true if the first number is greater than the second.*/
